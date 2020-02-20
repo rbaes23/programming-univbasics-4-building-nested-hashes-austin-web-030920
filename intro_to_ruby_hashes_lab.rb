0371b92b9e842a5b2c40c8420b40642b0e97c05a
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-20 23:47:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,9 @@ def monopoly_with_third_tier
       :three_pieces_owned => 100,
       :four_pieces_owned => 200},
     :names => {
-      :reading_railroad => {},
+      :reading_railroad => {
+        "mortgage value" => $
+      },
       :pennsylvania_railroad => {}, 
       :b_and_o_railroad => {},
       :shortline_railroad => {}}
